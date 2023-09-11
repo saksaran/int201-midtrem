a968083265b7536ba13d90771559dba4b053f695
damn <110712863+saksaran@users.noreply.github.com>
2023-09-12 00:33:52 +0700

    Add files via upload

---
+++ objectAndArray/object/README.md
@@ -0,0 +1,4 @@
+### Object
+- call **“prototype base”**
+- is collection of properties
+- 1 properties contains `key` and `value`
---
+++ objectAndArray/object/object.js
@@ -0,0 +1,9 @@
+let object = {
+		name : "Josh",
+		id : "007",
+		email : "hello.home@gmail.com"
+}
+
+console.log(object.name);
+console.log(object.id);
+console.log(object.email);
